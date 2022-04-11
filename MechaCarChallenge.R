67dd79bff44957bf03cdcd8508d33fd4349c5539
Lucas <lucasbratland@gmail.com>
2022-04-10 19:05:58 -0500

    updated README-deliverable 1 and R file after delvierable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,13 @@
 library(tidyverse)
+
+#Deliverable 1
 mechacar <- read.csv("Resources/MechaCar_mpg.csv")
 head(mechacar)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar))
+
+
+#Deliverable 2
+suspen <- read.csv("Resources/Suspension_Coil.csv")
+total_summary <- suspen %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep')
+lot_summary <- suspen %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep')
--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Linear Regression to Predict MPG
 ![Image](/Resources/Images/Linear_Regression_Predict_MPG_1.PNG)
 ![Image2](/Resources/Images/Linear_Regression_Predict_MPG_2.PNG)
+
+- The main variables we would want to focus on are the intercept (which is significant but should factor into the variablity), vehicle length and ground clearance. 
+
+- The slope is non-zero. This is understood because the p-value of 5.35 e-11 is less than 0.05
+
+- The linear model does predict the mpg effectively. The r-squared value of .7149, means that the linear model accounts for 71& of the variability in the mpg. 
+
